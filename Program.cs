298a0c5bc4bb6e44960961785a248702beb5c436
Shivasahu123 <shivasahu577@gmail.com>
2024-08-20 22:08:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the height of the triangle: ");
+        int height = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = 0; j <= height-i; j++)
+            {
+                Console.Write(" ");
+            }
+            for(int k=0;k<(2*i-1);k++){
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
